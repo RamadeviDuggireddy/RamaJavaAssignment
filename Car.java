ebb714353942f70f3686cef73d6eb909d459f64a
RamadeviDuggireddy <62684247+RamadeviDuggireddy@users.noreply.github.com>
2022-04-11 11:06:42 -0400

    I added my  java simple program here

---
+++ Car.java
@@ -0,0 +1,20 @@
+package Mar22.week1.day1;
+
+public class Car {
+
+	public static void main(String[] args) {
+		Car Toyoto = new Car();
+		Toyoto.applyBreak();
+		Toyoto.applyHonk();
+		
+	}
+		
+public void  applyBreak()
+{
+	System.out.println("This is applyBreak");
+}
+public void  applyHonk()
+{
+System.out.println("This is applyHonk");
+}
+}
